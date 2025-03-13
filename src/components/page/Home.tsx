fe0abc405839966f509282cf8a509038452f02ab
s2s2hyun <s2s2hyun0601@gmail.com>
2025-03-13 17:50:11 +0900

    [Fix] change

--- src/components/page/Home.tsx
+++ src/components/page/Home.tsx
@@ -147,7 +147,18 @@ export default function Home() {
       <div className="main-top-section">
         <div className="main-top-content-wrap">
           <div className="main-top-left-wrap">
-            <img src={"/main-logo.png"} alt="sface-main" />
+            <img
+              src={"/main-top/ufo.gif"}
+              alt="sface-main"
+              style={{ width: "50%", minWidth: "340px" }}
+            />
+            {/* <video
+              src={"/main-top/main-logo.mp4"}
+              autoPlay
+              loop
+              muted
+              playsInline
+            /> */}
             <p>
               언제 어디서나 아티스트의 순간을 스트리밍 <br />
               내가 찍은 나의 최애, 즉시 프린트해 간직하세요!
