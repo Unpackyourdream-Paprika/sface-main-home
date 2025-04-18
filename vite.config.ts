32437ba020b7d954805455247ecf2a4454146ba6
nkj960822 <nkj960822@gmail.com>
2025-04-18 18:17:46 +0900

    fix:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Sface App",
+        short_name: "Sface",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#AE62EE",
+        icons: [
+          {
+            src: "/bottom-logo-sface.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/bottom-logo-sface.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
